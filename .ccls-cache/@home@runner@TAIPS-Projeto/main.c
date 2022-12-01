385252c4f657a397119d8d4704268f2934229a6e
Diogo Santos <53911973+diogodeese@users.noreply.github.com>
2022-12-01 04:45:41 +0000

    File Organization

--- .ccls-cache/@home@runner@TAIPS-Projeto/main.c
+++ .ccls-cache/@home@runner@TAIPS-Projeto/main.c
@@ -1,19 +1,72 @@
 #include "taips.h"
-#include <stdio.h>
 
-int main(void) {
-  srand(time(NULL));
+int main() {
+  int columns = getCoords('C'), rows = getCoords('R');
+  int position = 1, keyPressed = 0, option = 0;
 
-  printf("Hello World\n");
-  struct aeroplane aero;
+  char options[12][30] = {"Aeroplane Details",
+                          "Aeroplane Status",
+                          "Listar Aeronaves Autorizacao",
+                          "List Aeroplanes",
+                          "Add Aeroplane",
+                          "Edit Aeroplane",
+                          "Delete Aeroplane",
+                          "Authorize Fligth",
+                          "Cancel Fligth",
+                          "Save Aeroplanes",
+                          "Quit"};
 
-  aero.status = true;
+  // gotoxy(columns / 2, rows / 2);
 
-  for (int i = 0; i < 30; i++) {
-    printf("%d\n", generateId());
+  hideCursor();
+  menu("TAIPS", options);
+  menuArrow(position);
+
+  while (1) {
+    keyPressed = getch();
+
+    // Key Handler
+    switch (keyPressed) {
+    // Enter
+    case 13:
+      option = position;
+      break;
+
+    // Arrow Up
+    case 72:
+      if (position > 1)
+        menuArrow(--position);
+      break;
+
+    // Arrow Down
+    case 80:
+      if (position < 11)
+        menuArrow(++position);
+      break;
+
+    default:
+      position = position;
+      break;
+    }
+
+    // Menu option
+    switch (option) {
+    case 11:
+      exit(0);
+      break;
+
+    default:
+      srand(time(NULL));
+
+      Aeroplane aero;
+
+      aero.id = generateId();
+      aero.capacity = 2;
+
+      /*printf("Id: %d\n", aero.id);
+      printf("Capacity: %d\n", aero.capacity);*/
+
+      break;
+    }
   }
-
-  return 0;
 }
-
-// Ideias de como comecar i guess
--- taips.h
+++ taips.h
@@ -1,29 +1,36 @@
-#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <conio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <windows.h>
 
-struct aeroplane {
-  int id;               // id unico
-  int capacity;         // (mínimo de 2 e máximo de 4)
-  bool status;          // True = Pronto | False = Autorização
-  int passengerList[4]; // TODO: Array?
-  char local[30];       // Local onde está
-  char destination[30]; // Destino
-  // int history[5]; o historico dos ultimos voos sera um array ent n sei o que
-  int flightCount;
-};
+typedef struct Aeroplanes {
+    int id;               // Unique ID
+    int capacity;         // (mínimo de 2 e máximo de 4)
+    bool status;          // True = Pronto | False = Autorização
+    int passengerList[4]; // TODO: Array?
+    char local[30];       // Local onde está
+    char destination[30]; // Destino
+    // int history[5];    // O historico dos ultimos voos sera um array ent n sei o que
+    int flightCount;      // Flight Count
+} Aeroplane;
 
-struct passenger {
-  int nif;       // id unico
-  char name[30]; // Nome
-};
+typedef struct Passengers {
+    int nif;              // Unique ID
+    char name[30];        // Name
+} Passenger;
 
-int generateId() { return rand() % 900000000 + 99999999; }
+int generateId() {
+    return rand() % 900000000 + 99999999;
+}
 
 void gotoxy(int x, int y) {
-  COORD coord;
-  coord.X = x;
-  coord.Y = y;
-  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
 int getCoords(char c) {
@@ -37,6 +44,7 @@ int getCoords(char c) {
     c = toupper(c);
     if(c == 'C') return columns;
     if(c == 'R') return rows;
+    return 0;
 }
 
 void hideCursor() {
@@ -45,4 +53,65 @@ void hideCursor() {
     info.dwSize = 100;
     info.bVisible = FALSE;
     SetConsoleCursorInfo(consoleHandle, &info);
+}
+
+void menu(char title[35], char options[12][30]) {
+
+    // Top Menu Bar
+    for(int i = 0; i < 45; i++) {
+        if(i == 0) {
+            printf("%c", 201);
+        } else if(i == 44) {
+            printf("%c", 187);
+        } else {
+            printf("%c", 205);
+        }
+    }
+
+    // Left Menu Bar
+    for(int i = 0; i < 15; i++) {
+        gotoxy(0, i + 1);
+        printf("%c", 186);
+    }
+
+    // Right Menu Bar
+    for(int i = 0; i < 15; i++) {
+        gotoxy(44, i + 1);
+        printf("%c", 186);
+    }
+
+    // Down Menu Bar
+    for(int i = 0; i < 45; i++) {
+        if(i == 0) {
+            printf("\n%c", 200);
+        } else if(i == 44) {
+            printf("%c", 188);
+        } else {
+            printf("%c", 205);
+        }
+    }
+
+    int rest = strlen(title) % 2;
+
+    // Title
+    gotoxy(22 - (strlen(title) / 2) - rest, 2);
+    printf("%s", title);
+
+    // Options List
+    for(int i = 0; i < 12; i++) {
+        gotoxy(5, 3 + i + 1);
+        printf("%s", options[i]);
+    }
+}
+
+void menuArrow(int position) {
+    // Clear arrow
+    for(int i = 0; i < 12; i++) {
+        gotoxy(3, 3 + i + 1);
+        printf(" ");
+    }
+
+    gotoxy(3, 3 + position);
+    printf(">");
+    gotoxy(0, 20);
 }
